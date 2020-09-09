9d8093c4dea2db96eecd7b9d8913717aa0e8e2e8
shun-shobon <shun.shobon@gmail.com>
2020-09-09 22:54:45 +0900

    feat: add guild member update event listener

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,10 @@ const getBotsHasRole = (
 
 dotenv.config();
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-if (DISCORD_TOKEN == null) {
-  throw new Error("DISCORD_TOKEN is not defined.");
+const TARGET_GUILD = process.env.TARGET_GUILD;
+const LOG_CHANNEL = process.env.LOG_CHANNEL;
+if (DISCORD_TOKEN == null || TARGET_GUILD == null || LOG_CHANNEL == null) {
+  throw new Error("DISCORD_TOKEN or TARGET_GUILD or LOG_CHANNEL is not defined.");
 }
 
 const WATCH_PERMISSIONS: Discord.PermissionResolvable = [
@@ -40,11 +42,21 @@ client.on("ready", () => {
   console.log("ready.");
 });
 
-client.on("message", async (message) => {
-  if (message.author.bot) return;
-  if (message.content === "!ping") {
-    await message.channel.send("pong");
-  }
+client.on("guildMemberUpdate", async (_oldUser, newUser) => {
+  if (newUser.guild.id !== process.env.TARGET_GUILD) return;
+  if (!newUser.user?.bot) return;
+  if (!newUser.permissions.any(WATCH_PERMISSIONS)) return;
+  const embed = new Discord.MessageEmbed()
+    .setTitle("Watch Dog警告")
+    .setDescription("破壊的な権限がBotに付与されました。")
+    .setColor("RED")
+    .setThumbnail(newUser.user.avatarURL() ?? "")
+    .addField("Bot名", newUser.nickname ?? newUser.user.username)
+    .addField("現在付与されている権限", newUser.permissions.toArray().join("\n"));
+  const logChannel = newUser.guild.channels.cache.get(LOG_CHANNEL) as
+    | Discord.TextChannel
+    | undefined;
+  await logChannel?.send(embed);
 });
 
 client.login(DISCORD_TOKEN).catch(console.error);
